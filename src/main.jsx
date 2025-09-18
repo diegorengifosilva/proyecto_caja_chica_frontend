55b6ad9042d23d9d1a02975dd6552f7dfc338518
Diego Rengifo <diegorengifosilva@gmail.com>
2025-09-18 03:15:26 -0500

    Correcion gits

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client.js";
+import ReactDOM from "react-dom/client";
 import App from "./pages/App.jsx";
 import "./styles/globals.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import './styles/globals.css';
-import "./index.css"
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
